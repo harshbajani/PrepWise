04b8098f309df06f0ce2289b873688fc5637d17a
harshbajani <harshbajani7@gmail.com>
2025-03-24 11:21:58 +0530

    Auth pages implementation with auth form

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import Authform from "@/components/Authform";
+import React from "react";
+
+const SignUp = () => {
+  return <Authform type="sign-up" />;
+};
+
+export default SignUp;
---
+++ components/FormField.tsx
@@ -0,0 +1,40 @@
+import {
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues> {
+  name: Path<T>;
+  control: Control<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = ({
+  name,
+  control,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{label}</FormLabel>
+        <FormControl>
+          <Input placeholder={placeholder} {...field} type={type} />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormField;
