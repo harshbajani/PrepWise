ba9c10ea099f5490777c5d15119da1bd55a83dc6
harshbajani <harshbajani7@gmail.com>
2025-03-25 15:23:37 +0530

    fix: update Firebase configuration to use environment variables for sensitive data

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,17 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCE3mLqUlZUcoISv_LGMtjne7ck8AbCKos",
-  authDomain: "prepwise-ff878.firebaseapp.com",
-  projectId: "prepwise-ff878",
-  storageBucket: "prepwise-ff878.firebasestorage.app",
-  messagingSenderId: "1002990358961",
-  appId: "1:1002990358961:web:3a51ddc7907a5161e86d90",
-  measurementId: "G-7PQRDKVBMB",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
