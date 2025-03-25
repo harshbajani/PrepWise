6dad83e75da7bf3f46703bdebd4cf52cc96ddf86
harshbajani <harshbajani7@gmail.com>
2025-03-25 11:50:29 +0530

    feat: enhance interview page with user-specific data and feedback functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,36 @@
-// import InterviewCard from "@/components/InterviewCard";
+import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
+import {
+  getCurrentUser,
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+  const [userInterviews, latestInterviews] = await Promise.all([
+    user?.id ? getInterviewsByUserId(user.id) : Promise.resolve(null),
+    user?.id ? getLatestInterviews({ userId: user.id }) : Promise.resolve(null),
+  ]);
+  const hasPastInterviews = (userInterviews ?? []).length > 0;
+  const hasUpcomingInterviews = (latestInterviews ?? []).length > 0;
   return (
     <>
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
-          <h2>Get Interview-Ready with AI-powered Practice & Feedback</h2>
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
           <p className="text-lg">
-            Practice on real interview question & get instant feedback.
+            Practice real interview questions & get instant feedback
           </p>
+
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
+
         <Image
           src="/robot.png"
           alt="robo-dude"
@@ -25,25 +39,52 @@ const page = () => {
           className="max-sm:hidden"
         />
       </section>
+
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
+
         <div className="interviews-section">
-          {/* {dummyInterviews.map((interview) => (
-            <InterviewCard key={interview.id} {...interview} />
-          ))} */}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
+
       <section className="flex flex-col gap-6 mt-8">
-        <h2>Take an Interview</h2>
+        <h2>Take Interviews</h2>
+
         <div className="interviews-section">
-          {/* {dummyInterviews.map((interview) => (
-            <InterviewCard key={interview.id} {...interview} />
-          ))} */}
-          <p>There are no interviews available</p>
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
     </>
   );
 };
 
-export default page;
+export default Page;
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -25,7 +25,6 @@ export async function signUp(params: SignUpParams) {
       success: true,
       message: "Account created successfully. Please sign in.",
     };
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     console.error("Error creating a user:", error);
     if (error.code === "auth/email-alread-exists") {
@@ -102,7 +101,47 @@ export async function getCurrentUser(): Promise<User | null> {
   }
 }
 
+export async function signOut() {
+  const cookieStore = await cookies();
+
+  cookieStore.delete("session");
+}
+
 export async function isAuthenticated() {
   const user = await getCurrentUser();
   return !!user;
+}
+
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where("finalized", "==", true)
+    .where("userId", "!=", userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+
+export async function getInterviewsByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection("interviews")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
 }
